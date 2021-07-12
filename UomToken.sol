37b332b1a7e7a156037a21aa24ac6c001d638a6b
gmixoulis <gmixoulis@gmail.com>
2021-07-12 06:38:58 +0300

    Update UomToken.sol

--- UomToken.sol
+++ UomToken.sol
@@ -134,7 +134,7 @@ contract UoMSupplyToken is ERC20Interface, Owned {
     // - Owner's account must have sufficient balance to transfer
     // - 0 value transfers are allowed
     // ------------------------------------------------------------------------
-    function transfer(address to, uint tokens) public returns (bool success) {
+    function transfer(address to, uint tokens) onlyOwner public returns (bool success) {
         balances[msg.sender] = balances[msg.sender].sub(tokens);
         balances[to] = balances[to].add(tokens);
         emit Transfer(msg.sender, to, tokens);
